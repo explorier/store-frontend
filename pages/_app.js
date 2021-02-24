0f3aa13e8949d7fc314ce7aa4e4edfb206d1307f
Paolo Matchett <paolomatchett@gmail.com>
2021-02-24 17:33:49 -0600

    Initial Setup

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import Page from "../components/Page";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import "../components/styles/nprogress.css";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
