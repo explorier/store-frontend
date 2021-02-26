c3e6c24c65c8450cc53b5b7a6a3680e7c076454c
Paolo Matchett <paolomatchett@gmail.com>
2021-02-25 20:09:30 -0600

    Nav styles

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
 
 export default function Nav() {
   return (
-    <nav>
-      <Link href="/products">products</Link>
-      <Link href="/sell">sell</Link>
-      <Link href="/orders">orders</Link>
-      <Link href="/account">account</Link>
-    </nav>
+    <NavStyles>
+      <Link href="/products">Products</Link>
+      <Link href="/sell">Sell</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
+    </NavStyles>
   );
 }
