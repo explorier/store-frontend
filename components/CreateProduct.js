0567254206c69a11a8cdae3bb57d812c1fc8e57d
Paolo Matchett <paolomatchett@gmail.com>
2021-03-01 17:27:42 -0600

    Error handling for product upload

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,5 +1,33 @@
 import Form from "./styles/Form";
 import useForm from "../lib/useForm";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import ErrorMessage from "./ErrorMessage";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are getting passed in? And what types are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -8,14 +36,26 @@ export default function CreateProduct() {
     description: "The best shooz",
     image: "",
   });
+
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
+        //submit the inputfields to the backend:
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <ErrorMessage error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
