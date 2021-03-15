837dc78119c53e7df934961a070607b1845b498b
Paolo Matchett <paolomatchett@gmail.com>
2021-03-15 13:27:21 -0500

    Request password reset component

---
+++ components/RequestReset.js
@@ -0,0 +1,63 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import useForm from "../lib/useForm";
+import Form from "./styles/Form";
+import { CURRENT_USER_QUERY } from "./User";
+import ErrorMessage from "./ErrorMessage";
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink( email: $email ) {
+      code
+      message
+    }
+  }
+`;
+export default function RequestReset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: "",
+  });
+
+  const [signup, { data, loading, error }] = useMutation(
+    REQUEST_RESET_MUTATION,
+    {
+      variables: inputs,
+      // refetch the currently logged in user
+      //refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
+  async function handleSubmit(e) {
+    e.preventDefault(); //stop the form from submitting
+    const res = await signup().catch(console.error);
+    console.log(res);
+    console.log(error);
+    resetForm();
+    //Send the email and password the the graphqlAPI
+  }
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Request A Password Reset</h2>
+      <ErrorMessage error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>
+            success! Check you email or a link!
+          </p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Request Reset</button>
+      </fieldset>
+    </Form>
+  );
+}
