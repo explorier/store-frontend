e3244a5685c91dd208630d0c1d721c916f644b9b
Paolo Matchett <paolomatchett@gmail.com>
2021-03-18 14:56:59 -0500

    Remove From Cart button

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -11,6 +11,10 @@ const BigButton = styled.button`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
     deleteCartItem(id: $id) {
@@ -22,6 +26,7 @@ const REMOVE_FROM_CART_MUTATION = gql`
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
+    update,
   });
   return (
     <BigButton
