b0d45b32a3fa24ee04a9b632cd1ad046bf9077ec
Paolo Matchett <paolomatchett@gmail.com>
2021-03-25 15:43:00 -0500

    Refetch cart after order complete & redirect to order page

--- components/Checkout.js
+++ components/Checkout.js
@@ -13,6 +13,7 @@ import { useMutation } from "@apollo/client";
 import SickButton from "./styles/SickButton";
 import { useRouter } from "next/dist/client/router";
 import { useCart } from "../lib/cartState";
+import { CURRENT_USER_QUERY } from "./User";
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -47,7 +48,10 @@ function CheckoutForm() {
   const router = useRouter();
   const { closeCart } = useCart();
   const [checkout, { error: graphQLError }] = useMutation(
-    CREATE_ORDER_MUTATION
+    CREATE_ORDER_MUTATION,
+    {
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
   );
   async function handleSubmit(e) {
     // 1. Stop the form from submitting and turn the loader one
@@ -78,8 +82,10 @@ function CheckoutForm() {
     console.log(order);
     // 6. Change the page to view the order
     router.push({
-      pathname: "/order",
-      query: { id: order.data.checkout.id },
+      pathname: `/order/[id]`,
+      query: {
+        id: order.data.checkout.id,
+      },
     });
     // 7. Close the cart
     closeCart();
